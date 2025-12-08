92f3a9ea24123776f4c3484495c6af4de0a93d96
Unity12 sungjae <sj070600@gmail.com>
2025-12-08 20:39:03 +0900

    feat: 게임매니저 스크립트

---
+++ Assets/01_Scripts/Managers/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public DataManager DataManager { get; private set; }
+
+    private void Awake()
+    {//싱글톤
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        DataManager = new DataManager();
+
+        DataManager.Initialize();
+    }
+}
